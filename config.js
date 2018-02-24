3275706c460b7e4fe8f3b863238004fceecf6925
Bill Broughton <wlbrough@gmail.com>
2018-02-24 09:26:14 -0500

    Clean up config

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  apiKey:
-    process.env.GEOCODING_API_KEY || "AIzaSyAjX7GOcWUFZvGMPueHDZcI188SDOg7NMc",
+  apiKey: process.env.GEOCODING_API_KEY,
   maxApiCallsPerSecond: 50,
   inputFile: "Addresses.csv",
   outputFile: "Geocode.csv"
