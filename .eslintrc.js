fd57a39907997b434a15238ff9304fd0cf2cbb4d
Bill Broughton <wlbrough@gmail.com>
2019-06-04 10:55:43 -0400

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,9 @@ module.exports = {
   extends: ["standard", "prettier"],
   plugins: ["standard", "promise", "prettier"],
   rules: {
-    "prettier/prettier": "error"
+    "prettier/prettier": "error",
+    "no-plusplus": "off",
+    "no-console": "off"
   },
   env: {
     node: true,
